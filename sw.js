dfa17951c484e6ddc49f4856bb72ed1b84d531fa
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2018-04-23 14:47:49 +0900

    cache in root also

--- sw.js
+++ sw.js
@@ -5,6 +5,18 @@ self.addEventListener('install', ()=>{
 self.addEventListener('fetch', (event) => {
   const req = event && event.request;
   const url = req.url;
-  console.log(`fetch: ${url}`);
-  return;
+  console.log(`fetch: ${url} (root)`);
+
+  event.respondWith(
+    caches.open('main').then((cache) => {
+      return cache.match(req).then((matching)=>{
+        if (matching) {
+          return matching;
+        }
+        return fetch(req).then((res)=>{
+          cache.put(req, res);
+          return res.clone();
+        })
+      })
+  }));
 });
