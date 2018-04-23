d7ed1d75a540e7c170590c625bd32cf1ae4eccbf
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2018-04-23 14:44:29 +0900

    cache with serviceWorker in foo dir

--- foo/sw.js
+++ foo/sw.js
@@ -6,5 +6,17 @@ self.addEventListener('fetch', (event) => {
   const req = event && event.request;
   const url = req.url;
   console.log(`fetch: ${url} (foo)`);
-  return;
+
+  event.respondWith(
+    caches.open('main').then((cache) => {
+      return cache.match(req).then((matching)=>{
+        if (matching) {
+          return matching;
+        }
+        return fetch(req).then((res)=>{
+          cache.put(req, res);
+          return res.clone();
+        })
+      })
+  }));
 });
